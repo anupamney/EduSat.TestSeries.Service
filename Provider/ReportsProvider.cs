a59e46102dcf92f930155a3a7bd98107915682d3
Anupam Shandilya <Anupam.Shandilya@mindbodyonline.com>
2024-08-25 04:14:00 -0700

    add discount functionality

--- Provider/ReportsProvider.cs
+++ Provider/ReportsProvider.cs
@@ -20,32 +20,32 @@ namespace EduSat.TestSeries.Service.Provider
             connection.Open();
 
             var command = connection.CreateCommand();
-            command.CommandText = "SELECT\r\n    sd.ID,\r\n    sch.name,\r\n    tea.contact,\r\n    sch.school_id,\r\n    tea.first_name,\r\n    tea.last_name,\r\n    tea.email_id,\r\n    sd.total_students,\r\n    pd.paid,\r\n    pd.TOTAL_PAYMENT,\r\n    pd.payment_status, pd.Discount_Percent, pd.Discounted_Price\r\n    sd.ACADEMIC_YEAR,\r\n    sch.DISTRICT,\r\n    cl.NAME as class_name,\r\n    tea.IS_PRINCIPAL\r\nFROM\r\n    scholarships_details sd\r\nLEFT JOIN\r\n    schools sch ON sch.school_id = sd.school_id\r\nLEFT JOIN\r\n    teachers tea ON sd.teacher_id = tea.id\r\nLEFT JOIN\r\n    class cl ON cl.id = sd.class_id\r\nLEFT JOIN\r\n    payment_details pd ON pd.scholarship_id = sd.id;";
+            command.CommandText = "SELECT\r\n    sd.ID,\r\n    sch.name,\r\n    tea.contact,\r\n    sch.school_id,\r\n    tea.first_name,\r\n    tea.last_name,\r\n    tea.email_id,\r\n    sd.total_students,\r\n    pd.paid,\r\n    pd.TOTAL_PAYMENT,\r\n    pd.payment_status,\r\n pd.Discount_Percent,\r\n pd.Discounted_Price,\r\n    sd.ACADEMIC_YEAR,\r\n    sch.DISTRICT,\r\n    cl.NAME as class_name,\r\n    tea.IS_PRINCIPAL\r\nFROM\r\n    scholarships_details sd\r\nLEFT JOIN\r\n    schools sch ON sch.school_id = sd.school_id\r\nLEFT JOIN\r\n    teachers tea ON sd.teacher_id = tea.id\r\nLEFT JOIN\r\n    class cl ON cl.id = sd.class_id\r\nLEFT JOIN\r\n    payment_details pd ON pd.scholarship_id = sd.id;";
             using (var reader = command.ExecuteReader())
             {
                 while (await reader.ReadAsync())
                 {
-                    var sd= new SchoolDetails
+                    var sd = new SchoolDetails
                     {
-                        Id = reader.GetInt32(reader.GetOrdinal("ID")),
-                        SchoolName = reader.GetString(reader.GetOrdinal("name")),
-                        SRN = reader.GetInt32(reader.GetOrdinal("school_id")),
-                        TeacherFirstName = reader.GetString(reader.GetOrdinal("first_name")),
-                        TeacherLastName = reader.GetString(reader.GetOrdinal("last_name")),
-                        TeacherEmail = reader.GetString(reader.GetOrdinal("email_id")),
-                        TeacherContact = reader.GetString(reader.GetOrdinal("contact")),
-                        TotalStudents = reader.GetInt32(reader.GetOrdinal("total_students")),
-                        TotalPayment = reader.GetDecimal(reader.GetOrdinal("total_payment")),
-                        TotalPaymentReceived = reader.GetDecimal(reader.GetOrdinal("paid")),
-                        PaymentStatus = reader.GetBoolean(reader.GetOrdinal("payment_status")),
-                        AcademicYear = reader.GetString(reader.GetOrdinal("ACADEMIC_YEAR")),
-                        District = reader.GetString(reader.GetOrdinal("DISTRICT")),
-                        ClassName = reader.GetString(reader.GetOrdinal("class_name")),
-                        IsPrincipal = reader.GetBoolean(reader.GetOrdinal("IS_PRINCIPAL")),
-                        Discount_Percent = reader.GetInt32(reader.GetOrdinal("Discount_Percent")),
-                        Discounted_Price = reader.GetDecimal(reader.GetOrdinal("Discounted_Price"))
-                     };
-                     schoolDetails.Add(sd);
+                        Id = !reader.IsDBNull(reader.GetOrdinal("ID")) ? reader.GetInt32(reader.GetOrdinal("ID")) : 0,
+                        SchoolName = !reader.IsDBNull(reader.GetOrdinal("name")) ? reader.GetString(reader.GetOrdinal("name")) : string.Empty,
+                        SRN = !reader.IsDBNull(reader.GetOrdinal("school_id")) ? reader.GetInt32(reader.GetOrdinal("school_id")) : 0,
+                        TeacherFirstName = !reader.IsDBNull(reader.GetOrdinal("first_name")) ? reader.GetString(reader.GetOrdinal("first_name")) : string.Empty,
+                        TeacherLastName = !reader.IsDBNull(reader.GetOrdinal("last_name")) ? reader.GetString(reader.GetOrdinal("last_name")) : string.Empty,
+                        TeacherEmail = !reader.IsDBNull(reader.GetOrdinal("email_id")) ? reader.GetString(reader.GetOrdinal("email_id")) : string.Empty,
+                        TeacherContact = !reader.IsDBNull(reader.GetOrdinal("contact")) ? reader.GetString(reader.GetOrdinal("contact")) : string.Empty,
+                        TotalStudents = !reader.IsDBNull(reader.GetOrdinal("total_students")) ? reader.GetInt32(reader.GetOrdinal("total_students")) : 0,
+                        TotalPayment = !reader.IsDBNull(reader.GetOrdinal("total_payment")) ? reader.GetDecimal(reader.GetOrdinal("total_payment")) : 0m,
+                        TotalPaymentReceived = !reader.IsDBNull(reader.GetOrdinal("paid")) ? reader.GetDecimal(reader.GetOrdinal("paid")) : 0m,
+                        PaymentStatus = !reader.IsDBNull(reader.GetOrdinal("payment_status")) ? reader.GetBoolean(reader.GetOrdinal("payment_status")) : false,
+                        AcademicYear = !reader.IsDBNull(reader.GetOrdinal("ACADEMIC_YEAR")) ? reader.GetString(reader.GetOrdinal("ACADEMIC_YEAR")) : string.Empty,
+                        District = !reader.IsDBNull(reader.GetOrdinal("DISTRICT")) ? reader.GetString(reader.GetOrdinal("DISTRICT")) : string.Empty,
+                        ClassName = !reader.IsDBNull(reader.GetOrdinal("class_name")) ? reader.GetString(reader.GetOrdinal("class_name")) : string.Empty,
+                        IsPrincipal = !reader.IsDBNull(reader.GetOrdinal("IS_PRINCIPAL")) ? reader.GetBoolean(reader.GetOrdinal("IS_PRINCIPAL")) : false,
+                        Discount_Percent = !reader.IsDBNull(reader.GetOrdinal("Discount_Percent")) ? reader.GetInt32(reader.GetOrdinal("Discount_Percent")) : 0,
+                        Discounted_Price = !reader.IsDBNull(reader.GetOrdinal("Discounted_Price")) ? reader.GetDecimal(reader.GetOrdinal("Discounted_Price")) : 0m
+                    };
+                    schoolDetails.Add(sd);
                 }
             }
             
